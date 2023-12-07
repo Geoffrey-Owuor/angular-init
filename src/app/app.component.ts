b8c323c976bb98e1f3f8c47551a438e09d102d5e
Geoffrey-Owuor <76515367+Geoffrey-Owuor@users.noreply.github.com>
2023-12-07 17:03:37 +0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { ServicesComponent } from "./services/services.component";
+import { AboutComponent } from "./about/about.component";
+import { HeaderComponent } from "./header/header.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, FooterComponent, ServicesComponent, AboutComponent, HeaderComponent, NavbarComponent, ContactComponent]
 })
 export class AppComponent {
   title = 'AngularSite';
 }
+
